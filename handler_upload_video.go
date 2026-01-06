a5eb88b409c8d415234f3305381fff9752f57dd8
wjseele <wjseele@pm.me>
2026-01-06 11:24:51 +0100

    First version with s3 uploading

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
@@ -37,5 +42,49 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 	if videoData.UserID != userID {
 		respondWithError(w, http.StatusUnauthorized, "User doesn't own video", err)
+	}
+
+	videoFile, videoFileHeader, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't form a video", err)
+	}
+	defer videoFile.Close()
+
+	videoCheck, _, err := mime.ParseMediaType(videoFileHeader.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't find content type", err)
+	}
+	if videoCheck != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Wrong content type", err)
+	}
+
+	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	_, err = io.Copy(tempFile, r.Body)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't write video to file", err)
+	}
+
+	tempFile.Seek(0, io.SeekStart)
+
+	newVideoKey := fmt.Sprintf("%s.mp4", videoIDString)
+
+	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         aws.String(newVideoKey),
+		Body:        tempFile,
+		ContentType: &videoCheck,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error uploading file to S3", err)
+	}
+
+	newUrl := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, newVideoKey)
+	videoData.VideoURL = &newUrl
+	err = cfg.db.UpdateVideo(videoData)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error updating video data in db", err)
 	}
 }
